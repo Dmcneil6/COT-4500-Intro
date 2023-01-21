f2e9d33f608e136bb29fb293c4d69069e5622ce3
Dmcneil6 <122490497+Dmcneil6@users.noreply.github.com>
2023-01-20 22:12:29 -0500

    Update intro_to_python.py

--- intro_to_python.py
+++ intro_to_python.py
@@ -1,4 +1,4 @@
-##________________________________
+##________________________
 
 import numpy as np
 matrix1 = np.eye(3)
@@ -12,3 +12,5 @@ matrix3 = np.eye(3)
 matrix3[matrix3 != 1] += 3
 matrix3 = np.delete(matrix3, -1, 1)
 print(matrix3)
+
+##This is the first assignment for Numerical Calculus to compile the code you would just need to run it through any compiler that can read JAVA then press "Enter" or "Compile"
